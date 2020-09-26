2269a6141b8d1862c171aef1567ed27c7f7b7aa8
Nis5l <kirchler123@gmail.com>
2020-09-26 20:43:42 +0200

    json posts

--- Client/client.js
+++ Client/client.js
@@ -4,12 +4,8 @@ function login(user, pass)
 {
     request.post(
         'http://127.0.0.1/',
-        { json: { username: 'Nissl', password: 'Pass' } },
-        function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log(body);
-            }
-        }
+        { json: { username: user, password: pass } },
     );
 }
-login();
+
+login("Nissl", "123");
