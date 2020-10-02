3489cb8ee7c8c64a78505000b33c7746eefb0a33
Nis5l <kirchler123@gmail.com>
2020-10-02 20:40:43 +0200

    added loading bar for pack

--- Website/resources/components.js
+++ Website/resources/components.js
@@ -9,10 +9,13 @@ class ProgressRing extends HTMLElement {
     this._root = this.attachShadow({mode: 'open'});
     this._root.innerHTML = `
       <svg
-
         height="${radius * 2}"
         width="${radius * 2}"
        >
+         <style>
+          margin: auto;
+          padding: auto;
+         </style>
          <circle
            stroke="white"
            stroke-dasharray="${this._circumference} ${this._circumference}"
@@ -20,13 +23,19 @@ class ProgressRing extends HTMLElement {
            stroke-width="${stroke}"
            fill="transparent"
            r="${normalizedRadius}"
-           cx="${radius}"
-           cy="${radius}"
+           cx= 50%
+           cy= 50%
         />
         <text font-size=32pt x="50%" y="50%" text-anchor="middle" fill="#fff" dy=".38em">30:10</text>
       </svg>
       
       <style>
+        svg
+        {
+          width: 100%;
+          height: 100%;
+          position: relative;
+        }
         circle {
           transition: stroke-dashoffset 0.35s;
           transform: rotate(-90deg);
