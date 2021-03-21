6dceb58fa3a1de90e3685d8dc66be31b4c769e00
Nis5l <kirchler123@gmail.com>
2021-03-21 13:32:49 +0000

    cache fix

--- Server/cache.js
+++ Server/cache.js
@@ -96,6 +96,7 @@ class Client {
 
 	addCardFriend(card) {
 		this.startDecay(this.time, this.callback);
+		if(this.friendinventory == undefined) return;
 		this.friendinventory.inventory.push(card);
 		this.sortInv(true);
 	}
@@ -174,7 +175,11 @@ class Client {
 		this.lastlevel = level;
 
 		var inv = this.inventory;
-		if (friend) inv = this.friendinventory.inventory;
+		if (friend)
+		{
+			if(this.friendinventory == undefined) return;
+			inv = this.friendinventory.inventory;
+		}
 
 		var ret = [];
 		var newinv = [];
