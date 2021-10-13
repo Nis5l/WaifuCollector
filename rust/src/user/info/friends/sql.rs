93897bd163d2a5ddcc655d3008d30fab6d940a82
Nis5l <kirchler123@gmail.com>
2021-10-13 08:56:32 +0200

    fix: unwrap

--- rust/src/user/info/friends/sql.rs
+++ rust/src/user/info/friends/sql.rs
@@ -10,17 +10,17 @@ pub async fn get_user_friends(sql: &Sql, sent: bool, user_id: i32) -> Result<Vec
         "WHERE friends.usertwo=? AND users.id=friends.userone;"
     };
 
-    let friends: Vec<FriendDb> = sqlx::query_as(
-        &format!("SELECT
+    let friends: Vec<FriendDb> = sqlx::query_as(&format!(
+            "SELECT
             users.username,
             friends.userone,
             friends.usertwo,
             friends.friendStatus
-         FROM friends, users
-         {}", where_clause))
+            FROM friends, users
+            {}", where_clause))
         .bind(user_id)
         .fetch_all(&mut con)
-        .await.unwrap();
+        .await?;
 
     Ok(friends)
 }
