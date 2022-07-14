0ec368cd7533c22c70cb5afd5ddefea4c7afacd4
Nis5l <kirchler123@gmail.com>
2022-07-14 16:48:49 +0200

    feat: id serde

--- server/src/shared/id.rs
+++ server/src/shared/id.rs
@@ -1,22 +1,21 @@
 use std::{fmt, convert, str};
-use serde::{Serialize, Deserialize};
+use serde::{Serialize, Deserialize, de::Visitor};
 use rocket::request::FromParam;
-use rocket::form::{self, FromForm};
+use rocket::form::FromForm;
 
 use crate::shared::crypto::random_string::generate_random_string;
 
-#[derive(Debug, Clone, sqlx::Type, Serialize, Deserialize, FromForm)]
+#[derive(Debug, Clone, sqlx::Type, Serialize, FromForm)]
 #[sqlx(transparent)]
-#[field(validate = to_lowercase())]
 pub struct Id(String);
-
-fn to_lowercase<'a>(str: &String) -> form::Result<'a, ()>{
-    Ok(())
-}
 
 impl Id {
     pub fn new(len: usize) -> Self {
         Self(generate_random_string(len))
+    }
+
+    pub fn set(&mut self, value: String) {
+        self.0 = value.to_lowercase();
     }
 }
 
@@ -48,7 +47,7 @@ impl PartialEq for Id {
 
 impl fmt::Display for Id {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Id({})", self.0)
+        write!(f, "{}", self.0)
     }
 }
 
@@ -65,5 +64,120 @@ impl<'a> FromParam<'a> for Id {
 
     fn from_param(param: &'a str) -> Result<Self, Self::Error> {
         Ok(Self::from(param))
+    }
+}
+
+struct IdVisitor;
+
+//https://docs.rs/serde/latest/src/serde/de/impls.rs.html#473
+impl<'de> Visitor<'de> for IdVisitor {
+    type Value = Id;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("an id")
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(Id::from(v.to_owned()))
+    }
+
+    fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(Id::from(v))
+    }
+
+    fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        match str::from_utf8(v) {
+            Ok(s) => Ok(Id::from(s.to_owned())),
+            Err(_) => Err(serde::de::Error::invalid_value(serde::de::Unexpected::Bytes(v), &self)),
+        }
+    }
+
+    fn visit_byte_buf<E>(self, v: Vec<u8>) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        match String::from_utf8(v) {
+            Ok(s) => Ok(Id::from(s)),
+            Err(e) => Err(serde::de::Error::invalid_value(
+                serde::de::Unexpected::Bytes(&e.into_bytes()),
+                &self,
+            )),
+        }
+    }
+}
+
+struct IdInPlaceVisitor<'a>(&'a mut Id);
+
+impl<'a, 'de> Visitor<'de> for IdInPlaceVisitor<'a> {
+    type Value = ();
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("a string")
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        self.0.set(String::from(v));
+        Ok(())
+    }
+
+    fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        *self.0 = Id::from(v);
+        Ok(())
+    }
+
+    fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        match str::from_utf8(v) {
+            Ok(s) => {
+                self.0.set(String::from(s));
+                Ok(())
+            }
+            Err(_) => Err(serde::de::Error::invalid_value(serde::de::Unexpected::Bytes(v), &self)),
+        }
+    }
+
+    fn visit_byte_buf<E>(self, v: Vec<u8>) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        match String::from_utf8(v) {
+            Ok(s) => {
+                *self.0 = Id::from(s);
+                Ok(())
+            }
+            Err(e) => Err(serde::de::Error::invalid_value(
+                serde::de::Unexpected::Bytes(&e.into_bytes()),
+                &self,
+            )),
+        }
+    }
+}
+
+impl<'a> Deserialize<'a> for Id {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+       where D: serde::Deserializer<'a> {
+        deserializer.deserialize_str(IdVisitor)
+    }
+
+    fn deserialize_in_place<D>(deserializer: D, place: &mut Self) -> Result<(), D::Error>
+       where D: serde::Deserializer<'a>, {
+        deserializer.deserialize_string(IdInPlaceVisitor(place))
     }
 }
