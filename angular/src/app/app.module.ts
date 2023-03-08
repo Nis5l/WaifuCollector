7dbe552162fa56da92f2b353f690d3791d383a1e
Nis5l <kirchler123@gmail.com>
2023-03-08 17:17:32 +0100

    chore: todo

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,3 +1,6 @@
+//TODO: implement some sort of abstract subscription service, bevause every subscription has to be unsubscribed on ngDestroy to avoid memory leaks
+//example: this.registerSubscription(test.subscrbe(x => console.log(x))); now the subscription should automatically be unsubscribed on destroy
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
